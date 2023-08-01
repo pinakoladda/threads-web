da684074e9c14ad445e31d467ec6618dce65b926
ninakoliada <pinakoladda@ya.ru>
2023-08-01 14:20:54 +0400

    feat: add buttons in navbar & create media query

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,28 @@
-import { IconAdd } from "../IconAdd";
-import { IconFavorite } from "../IconFavorite";
-import { IconHome } from "../IconHome";
-import { IconProfile } from "../IconProfile";
-import { IconSearch } from "../IconSearch";
+import { IconAdd } from "../Icons/IconAdd";
+import { IconHome } from "../Icons/IconHome";
+import { IconSearch } from "../Icons/IconSearch";
+import { IconFavorite } from "../Icons/IconFavorite";
+import { IconProfile } from "../Icons/IconProfile";
 import "./index.css";
 
 export const Navbar = () => {
   return (
     <div className="navbar">
-      <IconHome />
-      <IconSearch />
-      <IconAdd />
-      <IconFavorite />
-      <IconProfile />
+      <button className="navbar__home-button button">
+        <IconHome />
+      </button>
+      <button className="navbar__search-button button">
+        <IconSearch />
+      </button>
+      <button className="navbar__add-button button">
+        <IconAdd />
+      </button>
+      <button className="navbar__favorite-button button">
+        <IconFavorite />
+      </button>
+      <button className="navbar__profile-button button">
+        <IconProfile />
+      </button>
     </div>
   );
 };
