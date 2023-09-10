c63b40837a3e61c0134a0304e9aa3b8962655af8
pinakoladda <pinakoladda8@gmail.com>
2023-09-10 13:58:30 +0400

    feat: styles for form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -16,19 +16,31 @@ export const LoginForm = () => {
   return (
     <div>
       <form className="form" onSubmit={onSubmit}>
-        <p className="form__name">Sign in to the account</p>
-        <input
-          className="form__input"
-          type="text"
-          name="login"
-          placeholder="Login"
-        />
-        <input
-          className="form__input"
-          type="password"
-          name="password"
-          placeholder="Password"
-        />
+        <p className="form__name">Sign in</p>
+        <div className="form__container">
+          <label for="login" className="form__label">
+            Login:
+          </label>
+          <input
+            id="login"
+            className="form__input"
+            type="text"
+            name="login"
+            placeholder="Login"
+          />
+        </div>
+        <div className="form__container">
+          <label for="password" className="form__label">
+            Password:
+          </label>
+          <input
+            id="password"
+            className="form__input"
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+        </div>
         <button className="button form__button" type="submit">
           Login
         </button>
