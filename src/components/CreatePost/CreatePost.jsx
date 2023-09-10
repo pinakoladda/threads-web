6ca7d50cc91c286c20ecae8d64b5fb4545af7908
pinakoladda <pinakoladda8@gmail.com>
2023-09-10 13:58:30 +0400

    feat: create-post request

--- src/components/CreatePost/CreatePost.jsx
+++ src/components/CreatePost/CreatePost.jsx
@@ -1,11 +1,20 @@
 import { Form } from "../Form";
 import { Button } from "../Button";
 import { IconAttach } from "../Icons/IconAttach";
+import { request } from "../../api";
 import "./index.css";
 
 export const CreatePost = () => {
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const text = event.target.text.value;
+    console.log(text);
+    request.post("/posts", { text }).then((res) => {
+      window.location.href = "/";
+    });
+  };
   return (
-    <Form className="create-post__form">
+    <Form className="create-post__form" onSubmit={onSubmit}>
       <div className="create-post__container">
         <Button className="create-post__close-button">Cancel</Button>
         <p className="create-post__header">New post</p>
@@ -14,7 +23,8 @@ export const CreatePost = () => {
         <div className="create-post__avatar"></div>
         <div className="container">
           <p className="create-post__user-name">buddy_good_boy</p>
-          <input
+          <textarea
+            name="text"
             role="textbox"
             className="create-post__input"
             placeholder="new thread here..."
