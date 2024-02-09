5beb3a6b0914f3edf642127f83667f9759053c91
pinakoladda <pinakoladda8@gmail.com>
2024-02-09 18:11:38 +0100

    feat: add styles for search page

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
 import { Button } from "../Button";
 import { Navbar } from "../Navbar/Navbar";
+import { PostsList } from "../PostsList/PostsList";
+import { useSearchParams } from "react-router-dom";
+import { request } from "../../api";
 import "./index.css";
 
 export const Search = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [query, setQuery] = useState(searchParams.get("query") || "");
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    request.get(`/posts?query=${query}`).then((res) => {
+      setPosts(res.data);
+    });
+  }, [query]);
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  useEffect(() => {
+    setSearchParams({ query });
+  }, [query]);
+
   return (
     <>
       <Navbar />
@@ -12,6 +34,8 @@ export const Search = () => {
         </label>
         <div className="search__container">
           <input
+            onChange={handleInputChange}
+            value={query}
             className="search__input"
             placeholder="I'm searching for..."
             id="mainInput"
@@ -19,6 +43,7 @@ export const Search = () => {
           />
           <Button className="search__button">Go!</Button>
         </div>
+        <PostsList posts={posts} />
       </div>
     </>
   );
